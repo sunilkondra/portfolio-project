cd4da00a7c3e53c1ebc9e616d99d1aa07d6c8173
sunilkondra <sunilkondra5@gmail.com>
2026-05-03 21:51:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ app = Flask(__name__)
 CORS(app)
 
 # Database Connection using Render Environment Variable
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+db_url = os.environ.get('DATABASE_URL')
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
